697e0a2c747992208c31a416665c48e97711c2f4
Jainal Gosaliya <jainal09gosalia@gmail.com>
2024-04-22 22:29:14 -0400

    docker compose up

--- frontend/constants.py
+++ frontend/constants.py
@@ -1,8 +1,11 @@
-# Constants for transcription
-STATUS_ENDPOINT = "http://localhost:8000/status"
+import os
 
-LOCAL_FILE__TRANSCRIPTION_ENDPOINT = "http://localhost:8000/transcribe-local"
-YOUTUBE_TRANSCRIPTION_ENDPOINT = "http://localhost:8000/transcribe-youtube"
-LIVE_TRANSCRIPTION_WEBSOCKET_ENDPOINT = "ws://localhost:8000/ws"
+HOST = os.getenv("HOST", "localhost")
+# Constants for transcription
+STATUS_ENDPOINT = f"http://{HOST}:8000/status"
+
+LOCAL_FILE__TRANSCRIPTION_ENDPOINT = f"http://{HOST}:8000/transcribe-local"
+YOUTUBE_TRANSCRIPTION_ENDPOINT = f"http://{HOST}:8000/transcribe-youtube"
+LIVE_TRANSCRIPTION_WEBSOCKET_ENDPOINT = f"ws://{HOST}:8000/ws"
 
 WHISPER_SAMPLING_RATE = 16000
